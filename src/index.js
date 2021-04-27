e28f8dfb523b68be77644352c6298ad37611213a
Bubesh P <hwalk958@gmail.com>
2021-04-27 22:06:10 +0530

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "@babel/polyfill";
-
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
-// import "./assets/index.css";
-
-ReactDOM.render(<App />, document.getElementById("App"));
